3e85ec89ec4e63138fa794f1c5eba43f94595677
arieshilmii <arieshilmii@gmail.com>
2026-09-11 11:21:13 +0700

    fix: load robust mobile camera initializer last

--- scanner_local.js
+++ scanner_local.js
@@ -1,6 +1,7 @@
-// Loader: scanner lokal capture-first, reader seven-segment, lalu buku simpanan per user.
+// Loader: scanner lokal capture-first, reader seven-segment, buku simpanan, lalu camera fix mobile.
 document.write('<script src="scanner_local_core.js"><\/script>');
 document.write('<script src="scanner_capture_patch.js"><\/script>');
 document.write('<script src="scanner_capture_v2.js"><\/script>');
 document.write('<script src="scanner_sevenseg_v3.js"><\/script>');
 document.write('<script src="scanner_book_v4.js"><\/script>');
+document.write('<script src="camera_fix_v5.js"><\/script>');
