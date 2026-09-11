1d3cdd64e0d72afb3d8cbe9983626bccfeba0f3c
arieshilmii <arieshilmii@gmail.com>
2026-09-11 22:14:25 +0700

    feat: enable glassmorphic UI v9 on design branch

--- scanner_local.js
+++ scanner_local.js
@@ -1,6 +1,7 @@
-// Loader: universal local OCR, per-user savings book, robust mobile camera, and scanner UI.
+// Loader: universal local OCR, per-user savings book, robust mobile camera, scanner UI, and glassmorphic v9 design.
 document.write('<script src="scanner_local_core.js"><\/script>');
 document.write('<script src="scanner_book_v4.js"><\/script>');
 document.write('<script src="camera_fix_v5.js"><\/script>');
 document.write('<script src="scanner_ui_v6.js"><\/script>');
 document.write('<script src="scanner_universal_v7.js"><\/script>');
+document.write('<script src="ui_glass_v9.js"><\/script>');
