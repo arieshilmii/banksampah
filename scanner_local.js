2444714c05c1960443f5ab45e9e06c1894fceab4
arieshilmii <arieshilmii@gmail.com>
2026-09-15 19:48:50 +0700

    fix: load direct jungle background layer

--- scanner_local.js
+++ scanner_local.js
@@ -6,3 +6,4 @@ document.write('<script src="scanner_ui_v6.js"><\/script>');
 document.write('<script src="scanner_universal_v7.js"><\/script>');
 document.write('<script src="ui_glass_v9.js"><\/script>');
 document.write('<script src="ui_glass_v10.js"><\/script>');
+document.write('<script src="ui_bg_fix_v11.js"><\/script>');
