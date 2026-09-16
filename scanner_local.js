dec6237ed16f4bf71e68e6b9f377ea380a0edcf4
arieshilmii <arieshilmii@gmail.com>
2026-09-16 14:43:47 +0700

    chore: load seven-segment fast OCR and warmed Tesseract without stale scripts

--- scanner_local.js
+++ scanner_local.js
@@ -3,11 +3,11 @@ document.write('<style id="clean-preload-v25">html,body{background:#ecfaf6!impor
 document.write('<script src="scanner_local_core.js?v=2503"><\/script>');
 document.write('<script src="scanner_book_v4.js?v=2503"><\/script>');
 document.write('<script src="scanner_ui_v6.js?v=2503"><\/script>');
-document.write('<script src="scanner_universal_v7.js?v=3101"><\/script>');
+document.write('<script src="scanner_universal_v7.js?v=3201"><\/script>');
 document.write('<script src="ui_home_v25.js?v=2503"><\/script>');
 document.write('<script src="search_autocomplete_v14.js?v=2503"><\/script>');
 document.write('<script src="ui_camera_v17.js?v=2503"><\/script>');
-document.write('<script src="camera_fix_v18.js?v=3102"><\/script>');
+document.write('<script src="camera_fix_v18.js?v=3201"><\/script>');
 document.write('<script src="ui_camera_v19.js?v=3101"><\/script>');
 document.write('<script src="ui_camera_v20.js?v=2503"><\/script>');
 document.write('<script src="ui_status_v21.js?v=2503"><\/script>');
