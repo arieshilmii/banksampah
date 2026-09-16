3e2144a10ac1723045f41337c60074d4bdb95503
arieshilmii <arieshilmii@gmail.com>
2026-09-16 14:25:53 +0700

    chore: load final camera initializer with fresh cache key

--- scanner_local.js
+++ scanner_local.js
@@ -7,7 +7,7 @@ document.write('<script src="scanner_universal_v7.js?v=3101"><\/script>');
 document.write('<script src="ui_home_v25.js?v=2503"><\/script>');
 document.write('<script src="search_autocomplete_v14.js?v=2503"><\/script>');
 document.write('<script src="ui_camera_v17.js?v=2503"><\/script>');
-document.write('<script src="camera_fix_v18.js?v=3101"><\/script>');
+document.write('<script src="camera_fix_v18.js?v=3102"><\/script>');
 document.write('<script src="ui_camera_v19.js?v=3101"><\/script>');
 document.write('<script src="ui_camera_v20.js?v=2503"><\/script>');
 document.write('<script src="ui_status_v21.js?v=2503"><\/script>');
