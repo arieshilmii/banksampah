7789bf51006f11d346d92fed3603ebb1830012a6
arieshilmii <arieshilmii@gmail.com>
2026-09-16 15:44:47 +0700

    chore: load corrected threshold-consensus OCR without cached script

--- scanner_local.js
+++ scanner_local.js
@@ -3,7 +3,7 @@ document.write('<style id="clean-preload-v25">html,body{background:#ecfaf6!impor
 document.write('<script src="scanner_local_core.js?v=2503"><\/script>');
 document.write('<script src="scanner_book_v4.js?v=2503"><\/script>');
 document.write('<script src="scanner_ui_v6.js?v=2503"><\/script>');
-document.write('<script src="scanner_universal_v7.js?v=3201"><\/script>');
+document.write('<script src="scanner_universal_v7.js?v=3202"><\/script>');
 document.write('<script src="ui_home_v25.js?v=2503"><\/script>');
 document.write('<script src="search_autocomplete_v14.js?v=2503"><\/script>');
 document.write('<script src="ui_camera_v17.js?v=2503"><\/script>');
